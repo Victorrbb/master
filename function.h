92982a96f99ee0b07a5c65037b6b2092a2f509f1
Victorrbb <145706397+Victorrbb@users.noreply.github.com>
2023-11-12 14:59:08 +0000

    adicionei as tres primeira funçõe e adicionei structs novas

---
+++ function.h
@@ -0,0 +1,18 @@
+//
+// Created by victo on 27/10/2023.
+//
+#ifndef PROJETO3_FUNCTION_H
+#define PROJETO3_FUNCTION_H
+
+#include "struct.h"
+int salvarTarefas();
+int carregarTarefas();
+int crietarefa(ListaDeTarefas *lt);
+int apaguetarefa(ListaDeTarefas *lt);
+int listetarefa(ListaDeTarefas *lt);
+
+
+
+#endif //PROJETO3_FUNCTION_H
+
+
---
+++ struct.h
@@ -0,0 +1,19 @@
+//
+#ifndef PROJETO3_STRUCT_H
+#define PROJETO3_STRUCT_H
+#define TOTAL_TAREFAS 100
+#endif //PROJETO3_STRUCT_H
+
+typedef struct {
+    int prioridade;
+    char descricao[300];
+    char categoria[100];
+    int estado;
+} tipoTarefa;
+
+typedef struct {
+    tipoTarefa tarefas[TOTAL_TAREFAS];
+    int qtd;
+} ListaDeTarefas;
+
+
